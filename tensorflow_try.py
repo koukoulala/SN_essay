3a9be536ab3983456b22b9d9f52ea6b10ef1565d
扣扣biubiubiu <714366968@qq.com>
2018-03-03 15:16:59 +0800

    6

--- tensorflow_try.py
+++ tensorflow_try.py
@@ -10,9 +10,9 @@ import numpy as np
 import tensorflow as tf
 
 # Data sets
-TRAINING = "tmp/bcspwr/train2.csv"
+TRAINING = "tmp/bcspwr/train3.csv"
 
-TEST = "tmp/bcspwr/test2.csv"
+TEST = "tmp/bcspwr/test3.csv"
 
 def main():
   #https://cloud.tencent.com/developer/article/1005381   csv文件首行前两列分别表示数据组的个数和每个数据组的特征数
@@ -30,7 +30,7 @@ def main():
   feature_columns = [tf.contrib.layers.real_valued_column("", dimension=8)]
 
   # Build 3 layer DNN with 10, 20, 10 units respectively.
-  classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,hidden_units=[10, 20, 10],n_classes=8,model_dir="/tmp/t2")
+  classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,hidden_units=[10, 20, 10],n_classes=8,model_dir="/tmp/t3")
 
   # Define the training inputs
   def get_train_inputs():
