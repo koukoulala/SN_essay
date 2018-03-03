6a588d2f1d002f2db8386888e39718b524f24f52
扣扣biubiubiu <714366968@qq.com>
2018-03-03 10:57:27 +0800

    first

---
+++ find_structure.py
@@ -0,0 +1,59 @@
+from numpy import *
+
+#寻找那10%的节点的邻居节点结构，以一个节点为例子，153来测试，度数最大
+# 其communite=3(并且为了有w4的结构，手动加了（35,33）边）
+
+#读取边表文件和communite放入list中
+def read_file(Lfile,Cfile):
+    L=[];LL=set();C={};
+    with open(Lfile,'r')as f:
+        for line in f.readlines():
+            line=[int(x) for x in line.split()]
+            L.append(line)
+            for i in line:
+                LL.add(i)
+    with open(Cfile,'r')as f:
+        for line in f:
+            (key,value)=[int(x) for x in line.split()]
+            C[key]=value
+    return L,C,LL
+
+#分别获得三种结构的个数,得到n行3列的二维数组，W[i][j]=k代表Communite=i的节点的wj结构有k个
+def find_stru(L,n,C,Tnode):
+    #新建一个n行4列的零矩阵
+    W=zeros([n,3],int16)
+    WW=zeros(n,int)
+
+    #新建一个内嵌列表，维数为n，每个内部列表长度未知,l=[[],[],[],[],[]]
+    l=[];i=0;
+    while i!=n:
+        l.append([])
+        i+=1
+    for i in L:
+        if i[1] == Tnode:
+        # 把153的直接前驱点记录在不同的communite里面
+            l[C[i[0]]].append(i[0])
+    #print("目标节点",Tnode,"的直接前驱情况:",l)
+
+    #一个类别一个类别的来计算不同结构数目，存储在W中,WW中是邻居节点的总个数
+    k=0
+    for ll in l:
+        if ll!=[]:
+            W[k][0]=len(ll)
+            for i in ll:
+                flag=0; #如果flag=0，也就是不存在w2结构
+                for j in L:
+                    if j[1]==i and C[j[0]]==k: # 前驱点有前驱,且类别也相同
+                        flag = 1;
+                        k2 = j[0]
+                        if k2 in ll:  # 前驱的前驱也是直接前驱，w3的结构
+                            W[k][2] += 1;
+                        else:
+                            W[k][1] += 1;
+                #if flag == 0:
+                    #W[k][0] += 1;
+        k+=1
+
+    #print("目标节点",Tnode,"有",n,"种类别，每个类别3种结构数目:")
+    #print(W)
+    return W
---
+++ test.py
@@ -0,0 +1,60 @@
+from find_structure import *
+import random
+from sklearn.cross_validation import train_test_split
+
+Lfile='data/facebook_4039/facebook_4039.txt';
+Cfile='data/facebook_4039/facebook_4039_communities.txt'
+#Lfile = 'data/biological_tim/biological_tim.txt'
+#Cfile='data/biological_tim/biological_tim_communities.txt'
+L,C,LL=read_file(Lfile,Cfile)
+#print("所有的节点，已去重:")
+#print(LL)
+
+#计算图有几个Communites
+n=0;k=[];
+for key in C:
+    if C[key] not in k:
+        k.append(C[key])
+        n+=1
+print("图的commnunite类型：",k)
+print("图的commnunite数目=",n)
+
+#把3种邻居结构分别保存到3个csv文件中，把总的保存到第四个
+train_node=LL
+train_data=zeros([len(train_node),n*3],int)
+train_data1=zeros([len(train_node),n],int)
+train_data2 = zeros([len(train_node), n], int)
+train_data3 = zeros([len(train_node), n], int)
+label_data=zeros(len(train_data),int)
+k=0;
+for Trnode in train_node:
+    W=find_stru(L,n,C,Trnode)
+    W2=W.reshape(n*3,order='C')
+    #print("转换成行向量",W2)#一行有n*3列
+    train_data[k]=W2
+    train_data1[k]=W[:,0]
+    train_data2[k] = W[:, 1]
+    train_data3[k]=W[:,2]
+    label_data[k]=C[Trnode]
+    k+=1
+
+train_data=c_[train_data,label_data]
+train_data1 = c_[train_data1, label_data]
+train_data2 = c_[train_data2, label_data]
+train_data3 = c_[train_data3, label_data]
+print(train_data,"\n",train_data1,"\n",train_data2,"\n",train_data3,"\n")
+
+#选取10%的节点作为测试，90%是训练数据节点
+trainX,testX=train_test_split(train_data,test_size=0.1,random_state=2)
+train1,test1=train_test_split(train_data1,test_size=0.1,random_state=2)
+train2,test2=train_test_split(train_data2,test_size=0.1,random_state=2)
+train3,test3=train_test_split(train_data3,test_size=0.1,random_state=2)
+
+savetxt("tmp/train_facebook.csv",trainX,fmt="%d",delimiter=",")
+savetxt("tmp/train_facebook1.csv", train1, fmt="%d", delimiter=",")
+savetxt("tmp/train_facebook2.csv", train2, fmt="%d", delimiter=",")
+savetxt("tmp/train_facebook3.csv", train3, fmt="%d", delimiter=",")
+savetxt("tmp/test_facebook.csv",testX,fmt="%d",delimiter=",")
+savetxt("tmp/test_facebook1.csv", test1, fmt="%d", delimiter=",")
+savetxt("tmp/test_facebook2.csv", test2, fmt="%d", delimiter=",")
+savetxt("tmp/test_facebook3.csv", test3, fmt="%d", delimiter=",")
