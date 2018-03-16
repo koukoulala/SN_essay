aaa49cc2c314457f3ab8f3d704cc6d97725a6f49
kou <714366968@qq.com>
2018-03-16 16:44:37 +0800

    k

--- para_try.py
+++ para_try.py
@@ -36,5 +36,5 @@ def test_para(filepath,w,n):
     return acc
 
 if __name__ == "__main__":
-    acc=test_para("tmp/with_node/bcspwr_node_test.csv",[0.86749744,1.0554432,0.38650352],8)
+    acc=test_para("tmp/with_node/bcspwr_node_test.csv",[0.86749,1.0554,0.38652],8)
     print("准确率为：",acc)
